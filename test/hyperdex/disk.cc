064361d0b11267c9e325bbfaca71bf272ca2dcdc
Robert Escriva <me@robescriva.com>
2011-06-03 16:39:37 -0400

    A test case which finds the bug from HEAD^

--- test/hyperdex/disk.cc
+++ test/hyperdex/disk.cc
@@ -73,4 +73,42 @@ TEST(DiskTest, Simple)
     ASSERT_EQ(hyperdex::NOTFOUND, d.get(e::buffer("key", 3), 11062368440904502521UL, &value, &version));
 }
 
+TEST(DiskTest, MultiPut)
+{
+    hyperdex::zero_fill("tmp-disk");
+    hyperdex::disk d("tmp-disk");
+    unlink("tmp-disk");
+    std::vector<e::buffer> value;
+    std::vector<uint64_t> value_hashes;
+    uint64_t version;
+
+    // Put one point.
+    version = 64;
+    value.push_back(e::buffer("value-one", 9));
+    value_hashes.push_back(13379687116905876655UL);
+    ASSERT_EQ(hyperdex::SUCCESS, d.put(e::buffer("one", 3), 0xe1001e63b5e57068L, value, value_hashes, version));
+
+    // Put a second point.
+    version = 128;
+    value.clear();
+    value_hashes.clear();
+    value.push_back(e::buffer("value-two-a", 11));
+    value_hashes.push_back(3863907495625339794);
+    value.push_back(e::buffer("value-two-a", 11));
+    value_hashes.push_back(1626659109720743906);
+    ASSERT_EQ(hyperdex::SUCCESS, d.put(e::buffer("two", 3), 0x4a38a1aea3a81e5f, value, value_hashes, version));
+
+    // Make sure we can get both.
+    ASSERT_EQ(hyperdex::SUCCESS, d.get(e::buffer("one", 3), 0xe1001e63b5e57068L, &value, &version));
+    ASSERT_EQ(64, version);
+    ASSERT_EQ(1, value.size());
+    ASSERT_TRUE(e::buffer("value-one", 9) == value[0]);
+
+    ASSERT_EQ(hyperdex::SUCCESS, d.get(e::buffer("two", 3), 0x4a38a1aea3a81e5f, &value, &version));
+    ASSERT_EQ(128, version);
+    ASSERT_EQ(2, value.size());
+    ASSERT_TRUE(e::buffer("value-two-a", 11) == value[0]);
+    ASSERT_TRUE(e::buffer("value-two-b", 11) == value[1]);
+}
+
 } // namespace
