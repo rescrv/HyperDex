4bdf23ebaf4e430fe87e2a0863c079828605a06a
Robert Escriva <me@robescriva.com>
2011-09-02 11:30:53 -0400

    Compute the entry size as startof(next) - this.

--- hyperdisk/shard.cc
+++ hyperdisk/shard.cc
@@ -324,12 +324,12 @@ hyperdisk :: shard :: copy_to(const coordinate& c, e::intrusive_ptr<shard> s)
 
         // Figure out how big the entry is.
         size_t entry_start = static_cast<uint32_t>(m_search_index[ent * 2 + 1]);
-        size_t entry_size = m_data_offset;
+        size_t entry_size = m_data_offset - entry_start;
 
         if (ent < SEARCH_INDEX_ENTRIES - 1)
         {
-            size_t next_entry_size = static_cast<uint32_t>(m_search_index[ent * 2 + 3]);
-            entry_size = next_entry_size > 0 ? next_entry_size : entry_size;
+            size_t next_entry_start = static_cast<uint32_t>(m_search_index[ent * 2 + 3]);
+            entry_size = next_entry_start > 0 ? next_entry_start - entry_start: entry_size;
         }
 
         // Copy the entry's data
