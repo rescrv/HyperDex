474e85e1276fef185f8a6081c3c9c31f4e9ce07d
Robert Escriva <me@robescriva.com>
2011-09-14 02:54:50 -0400

    Remove useless funcs in hyperspace.h.

--- hyperdex/hyperdex/hyperspace.h
+++ hyperdex/hyperdex/hyperspace.h
@@ -43,42 +43,6 @@
 namespace hyperdex
 {
 
-// A spacing of 1 corresponds to the identity function.  A spacing of 0 is
-// undefined.  A spacing > 64 is undefined.
-inline uint64_t
-spacing(uint64_t number, uint8_t spacing)
-{
-    uint64_t ret = 0;
-
-    for (uint8_t i = 0; i * spacing < 64; ++i)
-    {
-        // Don't do this inline so that it will use the full 64-bit int.
-        // It's also much easier to read this way.
-        uint64_t bit = 1;
-        bit <<= (63 - i);
-        bit &= number;
-        bit >>= (i * (spacing - 1));
-        ret |= bit;
-    }
-
-    return ret;
-}
-
-// Interlace in a modified morton order (high-order bits preserved).
-inline uint64_t
-interlace(const std::vector<uint64_t>& nums)
-{
-    uint64_t ret = 0;
-
-    for (size_t i = 0; i < nums.size(); ++i)
-    {
-        uint64_t spaced = spacing(nums[i], nums.size());
-        ret |= (spaced >> i);
-    }
-
-    return ret;
-}
-
 inline uint64_t
 prefixmask(uint8_t prefix)
 {
@@ -120,56 +84,6 @@ nonoverlapping(const T& regions, R region)
     return true;
 }
 
-namespace hyperspace
-{
-
-inline void
-point_hashes(const e::buffer& key, const std::vector<e::buffer>& value,
-             uint64_t* key_hash, std::vector<uint64_t>* value_hashes)
-{
-    *key_hash = CityHash64(static_cast<const char*>(key.get()), key.size());
-
-    for (size_t i = 0; i < value.size(); ++i)
-    {
-        value_hashes->push_back(CityHash64(static_cast<const char*>(value[i].get()), value[i].size()));
-    }
-}
-
-inline uint32_t
-primary_point(uint64_t key_hash)
-{
-    return 0xffffffff & key_hash;
-}
-
-inline uint32_t
-secondary_point(const std::vector<uint64_t>& value_hashes)
-{
-    return 0xffffffff & interlace(value_hashes);
-}
-
-inline uint64_t
-replication_point(uint64_t key_hash, const std::vector<uint64_t>& value_hashes, const std::vector<bool>& which_dims)
-{
-    assert(which_dims.size() == value_hashes.size() + 1);
-    std::vector<uint64_t> hashes;
-
-    if (which_dims[0])
-    {
-        hashes.push_back(key_hash);
-    }
-
-    for (size_t i = 0; i < value_hashes.size(); ++i)
-    {
-        if (which_dims[i + 1])
-        {
-            hashes.push_back(value_hashes[i]);
-        }
-    }
-
-    return interlace(hashes);
-}
-
-} // namespace hyperspace
 } // namespace hyperdex
 
 #endif // hyperdex_hyperspace_h_
