1cb3e93b790f5fe88d3c575b774cc6951bb38c0a
Robert Escriva <me@robescriva.com>
2011-09-15 13:09:15 -0400

    Have network-workers trickle to disk more often.

--- hyperdaemon/network_worker.cc
+++ hyperdaemon/network_worker.cc
@@ -252,7 +252,7 @@ hyperdaemon :: network_worker :: run()
             {
                 ++count;
 
-                if (count % 100000 == 0)
+                if (count % 10000 == 0)
                 {
                     m_data->trickle(to.get_region());
                 }
