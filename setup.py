0b28bf4c88305ac3062182f818644ac8196d840b
Robert Escriva <me@robescriva.com>
2011-12-05 15:08:57 -0500

    Install the hdcoordinator Python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='HDCoordinator',
+      version='0.1.0',
+      author='Robert Escriva',
+      author_email='escriva@cs.cornell.edu',
+      packages=['hdcoordinator'],
+      url='http://hyperdex.org/',
+      license='3-clause BSD',
+      description='The coordinator for HyperDex'
+)
