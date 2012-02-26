036f8df8760f6d21b61e4737dfbbdc8d238f0b45
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-02-26 12:46:47 +0100

    gentrace.py: make compatible with python3, add shebang and move to tools/
    Signed-off-by: Serge Zirukin <ftrvxmtrx@gmail.com>

--- tools/gentrace.py
+++ tools/gentrace.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import math
 import random
 import string
@@ -11,9 +13,9 @@ keys = []
 
 for i in range(NUMOPS):
     if keys and random.random() < PERCGET:
-        print 'GET', random.choice(keys)
+        print(str.join(' ', [ 'GET', random.choice(keys) ]))
     else:
         key = ''.join([random.choice(charset) for i in range(8)])
         value = ''.join([random.choice(charset) for i in range(64)])
         keys.append(key)
-        print 'PUT', key, 'value', value
+        print(str.join(' ', [ 'PUT', key, 'value', value ]))
