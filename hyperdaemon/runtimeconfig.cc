d6d8c8341f174aa70a5600f26c596e7e1d88a89d
Robert Escriva <me@robescriva.com>
2012-02-29 13:22:55 -0500

    Reduce the number of in-flight transfers.

--- hyperdaemon/runtimeconfig.cc
+++ hyperdaemon/runtimeconfig.cc
@@ -32,6 +32,6 @@ e::envconfig<unsigned int> hyperdaemon::PREALLOCATIONS_PER_SECOND("HYPERDEX_PREA
 e::envconfig<unsigned int> hyperdaemon::OPTIMISM_BURSTS_PER_SECOND("HYPERDEX_OPTIMISM_BURSTS_PER_SECOND", 2);
 e::envconfig<unsigned int> hyperdaemon::FLUSH_THREADS("HYPERDEX_FLUSH_THREADS", 4);
 e::envconfig<size_t> hyperdaemon::LOCK_STRIPING("HYPERDEX_LOCK_STRIPING", 1024);
-e::envconfig<size_t> hyperdaemon::TRANSFERS_IN_FLIGHT("HYPERDEX_TRANSFERS_IN_FLIGHT", 1000);
+e::envconfig<size_t> hyperdaemon::TRANSFERS_IN_FLIGHT("HYPERDEX_TRANSFERS_IN_FLIGHT", 8);
 e::envconfig<uint16_t> hyperdaemon::REPLICATION_HASHTABLE_SIZE("HYPERDEX_REPLICATION_HASHTABLE_SIZE", 10);
 e::envconfig<uint16_t> hyperdaemon::STATE_TRANSFER_HASHTABLE_SIZE("HYPERDEX_STATE_TRANSFER_HASHTABLE_SIZE", 10);
