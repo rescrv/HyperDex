d5ec6eeea4e253934edd3618639bf550b075f6ef
Robert Escriva <me@robescriva.com>
2012-03-05 04:22:48 -0500

    Use popt for the daemon

--- daemon.cc
+++ daemon.cc
@@ -30,6 +30,10 @@
 
 // POSIX
 #include <signal.h>
+#include <sys/stat.h>
+
+// popt
+#include <popt.h>
 
 // Google Log
 #include <glog/logging.h>
@@ -37,36 +41,168 @@
 // po6
 #include <po6/error.h>
 
+// e
+#include <e/guard.h>
+
 // HyperDex
 #include "hyperdaemon/hyperdaemon/daemon.h"
 
-int
-usage();
+static const char* data = ".";
+static const char* host = "127.0.0.1";
+static po6::net::ipaddr coord(host);
+static long port = 1234;
+static long threads = 1;
+static const char* bindto = "127.0.0.1";
+static po6::net::ipaddr local(bindto);
+static long port_in = 0;
+static long port_out = 0;
+
+extern "C"
+{
+
+static struct poptOption popts[] = {
+    POPT_AUTOHELP
+    {"data", 'd', POPT_ARG_STRING, &data, 'd',
+        "the local directory in which data will be stored",
+        "D"},
+    {"host", 'h', POPT_ARG_STRING, &host, 'h',
+        "the IP address of the coordinator",
+        "IP"},
+    {"port", 'p', POPT_ARG_LONG, &port, 'p',
+        "the port number of the coordinator",
+        "port"},
+    {"threads", 't', POPT_ARG_LONG, &threads, 't',
+        "the number of threads which will handle network traffic",
+        "N"},
+    {"bind-to", 'b', POPT_ARG_STRING, &bindto, 'b',
+        "the local IP address that all sockets should bind to",
+        "IP"},
+    {"incoming-port", 'i', POPT_ARG_LONG, &port_in, 'i',
+        "the port to listen on for incoming connections",
+        "port"},
+    {"outgoing-port", 'o', POPT_ARG_LONG, &port_out, 'o',
+        "the port to use for outgoing connections",
+        "port"},
+    POPT_TABLEEND
+};
+
+} // extern "C"
 
 int
 main(int argc, char* argv[])
 {
+    poptContext poptcon;
+    poptcon = poptGetContext(NULL, argc, (const char**) argv, popts, POPT_CONTEXT_POSIXMEHARDER);
+    e::guard g = e::makeguard(poptFreeContext, poptcon);
+    g.use_variable();
+    int rc;
+    struct stat stbuf;
+
+    while ((rc = poptGetNextOpt(poptcon)) != -1)
+    {
+        switch (rc)
+        {
+            case 'd':
+                if (stat(data, &stbuf) < 0)
+                {
+                    std::cerr << "could not access data directory:  " << strerror(errno) << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 'h':
+                try
+                {
+                    coord = po6::net::ipaddr(host);
+                }
+                catch (po6::error& e)
+                {
+                    std::cerr << "cannot parse coordinator address" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                catch (std::invalid_argument& e)
+                {
+                    std::cerr << "cannot parse coordinator address" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 'p':
+                if (port < 0 || port >= (1 << 16))
+                {
+                    std::cerr << "coordinator port number out of range for TCP" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 't':
+                if (threads <= 0)
+                {
+                    std::cerr << "cannot create a non-positive number of threads" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                else if (threads > 512)
+                {
+                    std::cerr << "refusing to create more than 512 threads" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 'b':
+                try
+                {
+                    local = po6::net::ipaddr(bindto);
+                }
+                catch (po6::error& e)
+                {
+                    std::cerr << "cannot parse bind-to as IP address" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                catch (std::invalid_argument& e)
+                {
+                    std::cerr << "cannot parse bind-to as IP address" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 'i':
+                if (port_in < 0 || port_in >= (1 << 16))
+                {
+                    std::cerr << "coordinator port number out of range for TCP" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case 'o':
+                if (port_out < 0 || port_out >= (1 << 16))
+                {
+                    std::cerr << "coordinator port number out of range for TCP" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                break;
+            case POPT_ERROR_NOARG:
+            case POPT_ERROR_BADOPT:
+            case POPT_ERROR_BADNUMBER:
+            case POPT_ERROR_OVERFLOW:
+                std::cerr << poptStrerror(rc) << " " << poptBadOption(poptcon, 0) << std::endl;
+                return EXIT_FAILURE;
+            case POPT_ERROR_OPTSTOODEEP:
+            case POPT_ERROR_BADQUOTE:
+            case POPT_ERROR_ERRNO:
+            default:
+                std::cerr << "logic error in argument parsing" << std::endl;
+                return EXIT_FAILURE;
+        }
+    }
+
     google::InitGoogleLogging(argv[0]);
     google::InstallFailureSignalHandler();
-
-    if (argc != 4 && argc != 5)
-    {
-        return usage();
-    }
 
     // Run the daemon.
     try
     {
-        po6::net::location coordinator = po6::net::location(argv[1], atoi(argv[2]));
-        po6::net::ipaddr bind_to = po6::net::ipaddr(argv[3]);
-        po6::pathname base = ".";
-
-        if (argc == 5)
-        {
-            base = argv[4];
-        }
-
-        return hyperdaemon::daemon(base, coordinator, 2, bind_to, 0, 0);
+        return hyperdaemon::daemon(data, po6::net::location(coord, port), threads, local, port_in, port_out);
     }
     catch (std::exception& e)
     {
@@ -74,12 +210,4 @@ main(int argc, char* argv[])
     }
 
     return EXIT_SUCCESS;
-}
-
-int
-usage()
-{
-    std::cerr << "Usage:  hyperdexd <coordinator ip> <coordinator port> <bind to> [<datadir>]"
-              << std::endl;
-    return EXIT_FAILURE;
 }
