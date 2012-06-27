9b3c2fe4bff3498272f71b6137f7ef14cfd17af6
Robert Escriva <me@robescriva.com>
2012-06-26 20:58:05 -0400

    Skip the "old" directory.

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ release = '0.5.development'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'api/shards']
+exclude_patterns = ['_build', 'api/shards', 'old']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
