833bfe5687b8bcc383f37a297b42e587dd06abb9
Robert Escriva <me@robescriva.com>
2013-12-09 16:08:58 -0500

    Update test env with RUBYLIB

--- test/env.sh
+++ test/env.sh
@@ -4,3 +4,4 @@ export HYPERDEX_VERSION="$3"
 
 export CLASSPATH="${HYPERDEX_BUILDDIR}"/bindings/java/org.hyperdex.client-${HYPERDEX_VERSION}.jar:"${HYPERDEX_BUILDDIR}"/test/java:
 export PYTHONPATH="$1"/bindings/python:"$1"/bindings/python/.libs:"$2"/bindings/python:"$2"/bindings/python/.libs:
+export RUBYLIB="$1"/bindings/ruby/.libs:"$2"/bindings/ruby/.libs
