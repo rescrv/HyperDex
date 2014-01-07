ac94f7e0013061f9fec383c5fb33f356f2998794
Robert Escriva <me@robescriva.com>
2014-01-07 00:31:42 -0500

    Set java.library.path

--- test/sh/bindings.java.DataTypeString.sh
+++ test/sh/bindings.java.DataTypeString.sh
@@ -2,4 +2,4 @@
 javac -d "${HYPERDEX_BUILDDIR}"/test/java "${HYPERDEX_SRCDIR}"/test/java/DataTypeString.java
 
 python "${HYPERDEX_SRCDIR}"/test/runner.py --space="space kv key k attributes v" --daemons=1 -- \
-    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs DataTypeString {HOST} {PORT}
+    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs:/usr/local/lib:/usr/local/lib64:/usr/lib:/usr/lib64 DataTypeString {HOST} {PORT}
