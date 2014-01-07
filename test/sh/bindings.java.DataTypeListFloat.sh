4360b3c0c399ae3ce47e75de712fdbaf33c8707d
Robert Escriva <me@robescriva.com>
2014-01-07 00:31:42 -0500

    Set java.library.path

--- test/sh/bindings.java.DataTypeListFloat.sh
+++ test/sh/bindings.java.DataTypeListFloat.sh
@@ -2,4 +2,4 @@
 javac -d "${HYPERDEX_BUILDDIR}"/test/java "${HYPERDEX_SRCDIR}"/test/java/DataTypeListFloat.java
 
 python "${HYPERDEX_SRCDIR}"/test/runner.py --space="space kv key k attributes list(float) v" --daemons=1 -- \
-    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs DataTypeListFloat {HOST} {PORT}
+    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs:/usr/local/lib:/usr/local/lib64:/usr/lib:/usr/lib64 DataTypeListFloat {HOST} {PORT}
--- test/sh/bindings.java.DataTypeMapFloatInt.sh
+++ test/sh/bindings.java.DataTypeMapFloatInt.sh
@@ -2,4 +2,4 @@
 javac -d "${HYPERDEX_BUILDDIR}"/test/java "${HYPERDEX_SRCDIR}"/test/java/DataTypeMapFloatInt.java
 
 python "${HYPERDEX_SRCDIR}"/test/runner.py --space="space kv key k attributes map(float, int) v" --daemons=1 -- \
-    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs DataTypeMapFloatInt {HOST} {PORT}
+    java -Djava.library.path="${HYPERDEX_BUILDDIR}"/.libs:/usr/local/lib:/usr/local/lib64:/usr/lib:/usr/lib64 DataTypeMapFloatInt {HOST} {PORT}
