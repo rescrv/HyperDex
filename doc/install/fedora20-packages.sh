77aa30e38fdcdb20d258155479d20dee78e0c9b5
Robert Escriva <robert@rescrv.net>
2014-10-22 23:46:56 -0400

    Add fedora 20 packages instructions

--- doc/install/fedora20-packages.sh
+++ doc/install/fedora20-packages.sh
@@ -5,11 +5,11 @@ set -e
 cat >> /etc/yum.conf << EOF
 [hyperdex]
 name=hyperdex
-baseurl=http://centos.hyperdex.org/base/\$basearch/\$releasever
+baseurl=http://fedora.hyperdex.org/base/\$basearch/\$releasever
 enabled=1
 gpgcheck=1
 EOF
 
-rpm --import http://centos.hyperdex.org/hyperdex.gpg.key
+rpm --import http://fedora.hyperdex.org/hyperdex.gpg.key
 yum install -y epel-release
 yum install -y hyperdex
