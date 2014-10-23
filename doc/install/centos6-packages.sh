a75b474a72600affbc3b9adeffdd063a632ce9e8
Robert Escriva <robert@rescrv.net>
2014-10-22 21:18:19 -0400

    Update CentOS instructions

--- doc/install/centos6-packages.sh
+++ doc/install/centos6-packages.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo >> /etc/yum.conf << EOF
+cat >> /etc/yum.conf << EOF
 [hyperdex]
 name=hyperdex
-baseurl=http://centos.hyperdex.org/base/$basearch/$releasever
+baseurl=http://centos.hyperdex.org/base/\$basearch/\$releasever
 enabled=1
+gpgcheck=1
 EOF
 
+rpm --import http://centos.hyperdex.org/hyperdex.gpg.key
+yum install -y epel-release
 yum install -y hyperdex
