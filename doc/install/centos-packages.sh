3c32f4ed85b91d4ba86a3d69de15aa9187a9f2bd
Robert Escriva <robert@rescrv.net>
2014-10-24 16:12:14 -0400

    Condense CentOS install instructions

--- doc/install/centos-packages.sh
+++ doc/install/centos-packages.sh
@@ -9,7 +9,6 @@ baseurl=http://centos.hyperdex.org/base/\$basearch/\$releasever
 enabled=1
 gpgcheck=1
 EOF
-
 rpm --import http://centos.hyperdex.org/hyperdex.gpg.key
 yum install -y epel-release
 yum install -y hyperdex
