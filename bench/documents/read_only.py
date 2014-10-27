c8a4a2b4ae92f16aa040c229cb1901963173dc96
Kai Mast <km676@cornell.edu>
2014-10-27 17:57:19 -0400

    Working simple benchmark

--- bench/documents/read_only.py
+++ bench/documents/read_only.py
@@ -1,11 +1,30 @@
 #! /usr/bin/python
 
 import sys
+import functools
+
+from benchmark import Benchmark
 from hyperdex.mongo import HyperDatabase, HyperSpace 
+
+def createDoc(id, numElems):
+        doc = {'_id' : id}
+
+        for i in range(numElems):
+                doc['elem' + str(i)] = i
+
+        return doc
 
 # Setup
 db = HyperDatabase(sys.argv[1], int(sys.argv[2]))
-db.space.insert('val', {'this': {'is' : { 'just' : { 'a' : { 'nested' : 'document' }}}}})
 
-# Run 
-db.space.get('val')
+# Run
+for n in range(100, 10000, 100):
+        db.bench.insert(createDoc('myval', n))
+
+        bench = Benchmark()
+        result = bench.run(functools.partial(db.bench.get, 'myval'), 1000)
+
+        print "Num Elements: " + str(n)
+        print result.averageTime
+
+        db.bench.remove('myval')
