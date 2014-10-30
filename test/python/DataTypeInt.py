2cf89ce39b93f32405e21596deaaed5a86a005af
Kai Mast <km676@cornell.edu>
2014-10-30 12:14:04 -0400

    Added some tests

--- test/python/DataTypeInt.py
+++ test/python/DataTypeInt.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python2
 import sys
 import hyperdex.client
+from testlib import *
 from hyperdex.client import LessEqual, GreaterEqual, LessThan, GreaterThan, Range, Regex, LengthEquals, LengthLessEqual, LengthGreaterEqual
+
 c = hyperdex.client.Client(sys.argv[1], int(sys.argv[2]))
 def to_objectset(xs):
     return set([frozenset(x.items()) for x in xs])
-assert c.put('kv', 'k', {}) == True
-assert c.get('kv', 'k') == {'v': 0}
-assert c.put('kv', 'k', {'v': 1}) == True
-assert c.get('kv', 'k') == {'v': 1}
-assert c.put('kv', 'k', {'v': -1}) == True
-assert c.get('kv', 'k') == {'v': -1}
-assert c.put('kv', 'k', {'v': 0}) == True
-assert c.get('kv', 'k') == {'v': 0}
-assert c.put('kv', 'k', {'v': 9223372036854775807}) == True
-assert c.get('kv', 'k') == {'v': 9223372036854775807}
-assert c.put('kv', 'k', {'v': -9223372036854775808}) == True
-assert c.get('kv', 'k') == {'v': -9223372036854775808}
+    
+assertTrue(c.put('kv', 'k', {}))
+assertEquals(c.get('kv', 'k'), {'v': 0})
+assertTrue(c.put('kv', 'k', {'v': 1}))
+assertEquals(c.get('kv', 'k'), {'v': 1})
+assertTrue(c.put('kv', 'k', {'v': -1}))
+assertEquals(c.get('kv', 'k'), {'v': -1})
+assertTrue(c.put('kv', 'k', {'v': 0}))
+assertEquals(c.get('kv', 'k'), {'v': 0})
+assertTrue(c.put('kv', 'k', {'v': 9223372036854775807}))
+assertEquals(c.get('kv', 'k'), {'v': 9223372036854775807})
+assertTrue(c.put('kv', 'k', {'v': -9223372036854775808}))
+assertEquals(c.get('kv', 'k'), {'v': -9223372036854775808})
+
+# Atomic Operations
+assertTrue(c.put('kv', 'k', {'v': -1}))
+assertEquals(c.get('kv', 'k'), {'v': -1})
+assertTrue(c.atomic_min('kv', 'k', {'v': -2}))
+assertEquals(c.get('kv', 'k'), {'v': -2})
+assertTrue(c.atomic_max('kv', 'k', {'v': 1}))
+assertEquals(c.get('kv', 'k'), {'v': 1})
+assertTrue(c.atomic_min('kv', 'k', {'v': 2}))
+assertEquals(c.get('kv', 'k'), {'v': 1})
+assertTrue(c.atomic_add('kv', 'k', {'v': 128}))
+assertEquals(c.get('kv', 'k'), {'v': 129})
+assertTrue(c.atomic_add('kv', 'k', {'v': -5}))
+assertEquals(c.get('kv', 'k'), {'v': 124})
