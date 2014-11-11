4bbe9766a24076c86db762207dd2a5312e73f1ed
Robert Escriva <robert@rescrv.net>
2014-11-11 13:35:17 -0500

    Preserve pythonpath in test env

--- test/env.sh
+++ test/env.sh
@@ -2,6 +2,6 @@ export HYPERDEX_SRCDIR="$1"
 export HYPERDEX_BUILDDIR="$2"
 export HYPERDEX_VERSION="$3"
 
-export CLASSPATH="${HYPERDEX_BUILDDIR}"/bindings/java/org.hyperdex.client-${HYPERDEX_VERSION}.jar:"${HYPERDEX_BUILDDIR}"/test/java:
-export PYTHONPATH="$2"/bindings/python:"$2"/bindings/python/.libs:
-export RUBYLIB="$1"/bindings/ruby/.libs:"$2"/bindings/ruby/.libs
+export CLASSPATH="${HYPERDEX_BUILDDIR}"/bindings/java/org.hyperdex.client-${HYPERDEX_VERSION}.jar:"${HYPERDEX_BUILDDIR}"/test/java:${CLASSPATH}
+export PYTHONPATH="$2"/bindings/python:"$2"/bindings/python/.libs:${PYTHONPATH}
+export RUBYLIB="$1"/bindings/ruby/.libs:"$2"/bindings/ruby/.libs:${RUBYLIB}
