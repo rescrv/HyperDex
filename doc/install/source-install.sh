34c715cfb2cc16a7df276fef0d2ba0c77b8e74eb
Robert Escriva <robert@rescrv.net>
2015-08-06 20:13:41 -0400

    Update source install instructions

--- doc/install/source-install.sh
+++ doc/install/source-install.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-TARBALL_PO6=libpo6-0.5.2
-TARBALL_E=libe-0.8.2
-TARBALL_BUSYBEE=busybee-0.5.2
-TARBALL_LEVELDB=hyperleveldb-1.2.2
-TARBALL_REPLICANT=replicant-0.6.4
-TARBALL_MACAROONS=libmacaroons-0.2.0
-TARBALL_TREADSTONE=libtreadstone-0.1.0
-TARBALL_HYPERDEX=hyperdex-1.5.0
+TARBALL_PO6=libpo6-0.9.dev
+TARBALL_E=libe-0.12.dev.tar.gz
+TARBALL_BUSYBEE=busybee-0.7.dev.tar.gz
+TARBALL_LEVELDB=hyperleveldb-1.2.2.tar.gz
+TARBALL_REPLICANT=replicant-0.8.dev.tar.gz
+TARBALL_MACAROONS=libmacaroons-0.3.0.tar.gz
+TARBALL_TREADSTONE=libtreadstone-0.3.dev.tar.gz
+TARBALL_HYPERDEX=hyperdex-1.8.dev.tar.gz
 
 for tarball in \
     ${TARBALL_PO6} ${TARBALL_E} ${TARBALL_BUSYBEE} \
     ${TARBALL_LEVELDB} ${TARBALL_REPLICANT} ${TARBALL_MACAROONS} \
     ${TARBALL_TREADSTONE} ${TARBALL_HYPERDEX}
 do
-    wget http://hyperdex.org/src/${tarball}.tar.gz
     tar xzvf ${tarball}.tar.gz
     pushd ${tarball}
     ./configure
     make
-    sudo make install
+    make install
     popd
 done
