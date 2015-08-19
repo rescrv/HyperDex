49e274b258d1b1d4706355a76d80d064508bab3e
Robert Escriva <robert@rescrv.net>
2015-08-18 20:39:11 -0400

    Set coord lib in test environment

--- test/env.sh
+++ test/env.sh
@@ -2,6 +2,9 @@ export HYPERDEX_SRCDIR="$1"
 export HYPERDEX_BUILDDIR="$2"
 export HYPERDEX_VERSION="$3"
 
+export HYPERDEX_EXEC_PATH="${HYPERDEX_BUILDDIR}"
+export HYPERDEX_COORD_LIB="${HYPERDEX_BUILDDIR}"/.libs/libhyperdex-coordinator
+
 export PATH=${HYPERDEX_BUILDDIR}:${HYPERDEX_SRCDIR}:${PATH}
 
 export CLASSPATH="${HYPERDEX_BUILDDIR}"/bindings/java/org.hyperdex.client-${HYPERDEX_VERSION}.jar:"${HYPERDEX_BUILDDIR}"/test/java:${CLASSPATH}
