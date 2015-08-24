7c6d0108ef34262393d6ae2d06ff7064812403a0
Robert Escriva <robert@rescrv.net>
2015-08-24 14:38:12 -0400

    make distcheck?

--- test/sh/replication.reverse.daemons=1.fault-tolerance=0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SPACE="space replication key int A attributes int B, int C subspace C subspace B create 1 partitions tolerate 0 failures"
-exec python2 "${HYPERDEX_SRCDIR}"/test/runner.py --daemons=1 --space="${SPACE}" -- \
-     "${HYPERDEX_BUILDDIR}"/test/replication-stress-test --quiet -n 1 -h {HOST} -p {PORT}
--- test/sh/replication.reverse.daemons=4.fault-tolerance=1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SPACE="space replication key int A attributes int B, int C subspace C subspace B create 4 partitions tolerate 1 failures"
-exec python2 "${HYPERDEX_SRCDIR}"/test/runner.py --daemons=4 --space="${SPACE}" -- \
-     "${HYPERDEX_BUILDDIR}"/test/replication-stress-test --quiet -n 4 -h {HOST} -p {PORT}
